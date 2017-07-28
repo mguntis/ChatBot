d03f78cc4f85046b4773ac824691e28f843fb1ba
mguntis <m.guntis@gmail.com>
2017-07-28 11:57:47 +0300

    Update

--- index.js
+++ index.js
@@ -12,24 +12,27 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    var speech = "";
-    var country = req.body.result && req.body.result.parameters && req.body.result.parameters.geoCountry ? req.body.result.parameters.geoCountry : "Seems like some problem. Speak again."
-    switch (country) {
-        case "United States of America":
-            speech = "If you need information about immigration to US please say YES";
-            break;
-        case "United Kingdom of Great Britain and Northern Ireland":
-            speech = "If you need information about immigration to UK please say YES";
-            break;
-        default:
-            speech = "We have't information about immigration to " + country;
-    }
+    if (req.body.result.metadata.intentName = "workpermit.immigrate_to") {
+        var speech = "";
+        var country = req.body.result && req.body.result.parameters && req.body.result.parameters.geoCountry ? req.body.result.parameters.geoCountry : "Seems like some problem. Speak again."
 
-    return res.json({
-        speech: speech,
-        displayText: speech,
-        source: 'my-first-chatbot'
-    });
+        switch (country) {
+            case "United States of America":
+                speech = "If you need information about immigration to US please say YES";
+                break;
+            case "United Kingdom of Great Britain and Northern Ireland":
+                speech = "If you need information about immigration to UK please say YES";
+                break;
+            default:
+                speech = "We have't information about immigration to " + country;
+        }
+
+        return res.json({
+            speech: speech,
+            displayText: speech,
+            source: 'my-first-chatbot'
+        });
+    }
 });
 
 restService.listen((process.env.PORT || 8000), function() {
